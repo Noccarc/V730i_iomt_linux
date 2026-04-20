25cda7022865e79c1a4cea0561049c228befcfac
samarahmad247 <samar.a@noccarc.com>
2026-04-20 19:15:49 +0530

    r8152 set to y

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 14 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Mon Jun 24 20:16:37 IST 2024"
+#define UTS_VERSION "#14 SMP PREEMPT Mon Apr 20 19:10:49 IST 2026"
 #define LINUX_COMPILE_BY "samar"
 #define LINUX_COMPILE_HOST "samar-lap"
 #define LINUX_COMPILER "gcc version 10.2.0 (GCC)"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "5.4.84-dey-ga4ed1297e662-dirty"
+#define UTS_RELEASE "5.4.84-dey+gd328d158f04a"
